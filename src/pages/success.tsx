cd3580a2e39bf02ee00c62325777f76e83d0dd43
Amanda McGivern <amanda@flexpa.com>
2022-06-01 16:57:57 -0400

    Updated success message

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -13,20 +13,26 @@ const LinkSuccess: NextPage = () => {
     }, [router, app]);
 
     return (
-        <div>
-            <div className=''>
-                Success, your health plan has been linked!
-                The access token and patient ID are now stored in the app context.
-            </div>
-            <div className='app-context'>
-                <div className='table-heading'>Patient ID</div>
-                <div className='code'>{app?.patient}</div>
-            </div>
-            <div className='app-context'>
-                <div className='table-heading'>Access Token</div>
-                <div className='code'>{app?.flexpaAccessToken}</div>
+        <div className='page-container'>
+            <div className='section-container'>
+                <h1>
+                    Success, your health plan has been linked!
+                </h1>
+                <div>
+                    The access token and patient ID are now stored in the AppContext.
+                    The application is ready to start making FHIR requests.
+                </div>
             </div>
             <div>
+                <div className='app-context'>
+                    <div className='table-heading'>Patient ID</div>
+                    <div className='code'>{app?.patient}</div>
+                </div>
+                <div className='app-context'>
+                    <div className='table-heading'>Access Token</div>
+                    <div className='code'>{app?.flexpaAccessToken}</div>
+                </div>
+
 
             </div>
 
