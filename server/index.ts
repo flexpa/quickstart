fe7d621ea0d72dbcf3882050a59dc8dc5b6ade46
Amanda McGivern <amanda@flexpa.com>
2022-06-06 15:41:48 -0400

    Move route into separate file

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,9 @@
 import express, { Express, Request, Response } from 'express';
 import bodyParser from "body-parser";
 import cors from "cors";
-import fetch from 'node-fetch';
-
+import routes from './routes/flexpa_api_token.js';
 import 'dotenv/config';
 const app = express();
-
-// require('dotenv').config();
 
 // The server and front-end server run on different origins (localhost:3000 and localhost:9000 respectively).
 // Without CORS handling, the browser will not allow the frontend to make requests to the backend.
@@ -16,27 +13,8 @@ app.use(cors());
 
 // pre-parse the JSON body when appropriate
 app.use(bodyParser.json());
+app.use("", routes);
 
-app.post("/flexpa-access-token", async (req: Request, res: Response) => {
-    const { publicToken } = req.body;
-    console.log("process.env.FLEXPA_PUBLIC_API_BASE_URL", process.env.FLEXPA_PUBLIC_API_BASE_URL, process.env.FLEXPA_API_SECRET_KEY)
-    // call Flexpa API's exchange endpoint to exchange your `publicToken` for an `access_token` and a `patient_id`
-    const resp = await fetch(`${process.env.FLEXPA_PUBLIC_API_BASE_URL}/link/exchange`, {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-        },
-        body: JSON.stringify({
-            public_token: publicToken,
-            secret_key: process.env.FLEXPA_API_SECRET_KEY,
-        }),
-    });
-
-    const { access_token: accessToken, patient_id: patientId } = await resp.json();
-
-    // send the `accessToken` and `patientId` back to the client
-    res.send({ accessToken, patientId });
-});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`);
